67a67b508bf2314c5d38d8a679e8bd9f6830b609
Barak Saidoff <barak.saidoff@gmail.com>
2019-08-21 15:01:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,31 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  let min = 1
+  let max = 100
+  let random = Math.floor(Math.random() * (max - min)) + min
+ 
+  var objecto = {
+     itemName: item,
+     itemPrice: random
+   }
+   
+   cart.push(objecto)
+   return `${cart[cart.length - 1].itemName} has been added to your cart.`
+
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    return `Your shopping cart is empty.`
+ 
+   
+  } else if (cart.length === 1){
+   
+    return `In your cart, you have ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
+  
+  
+}
 }
 
 function total() {
