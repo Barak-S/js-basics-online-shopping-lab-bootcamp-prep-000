f3bb3a2512ba9d01dd335db74ec9b998df35c03d
Barak Saidoff <barak.saidoff@gmail.com>
2019-08-21 15:35:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,6 +58,8 @@ function removeFromCart(item) {
     if (cart[i].itemName === item){
       cart.splice(i,1);
       return cart;
+    } else if (cart[i].itemName !== item){
+      
     }
   }
   return "That item is not in your cart.";
